a2dda3219072ae9e2e08c5b5b27940e47c16121d
marca e deixa <85420527+marcaedeixa@users.noreply.github.com>
2026-09-14 15:43:17 -0300

    Add files via upload

--- libraries and code organization.R
+++ libraries and code organization.R
@@ -7,12 +7,17 @@ library(metatools)
 library(writexl)
 library(xportr)
 library(admiral)
+library(cards)
+library(gtsummary)
+library(haven)
+
 # codes to run, in order
 
 ## Specify directories as needed
 dir_raw = "rawdata/"
 dir_sdtm = "sdtm/"
 dir_adam = "adam/"
+dir_tlg = "tlg/"
 
 source(paste0(dir_raw, "Raw data.R"))
 source(paste0(dir_sdtm, "Controlled terminology.R"))
@@ -22,3 +27,5 @@ source(paste0(dir_sdtm, "lb.R"))
 source(paste0(dir_sdtm, "ae.R"))
 source(paste0(dir_sdtm, "vs.R"))
 source(paste0(dir_adam, "metacore.R"))
+source(paste0(dir_adam, "ADSL.R"))
+source(paste0(dir_tlg, "demographics.R"))
